d8f36bd1e3d4307dbd4741e53eae19127a8a093f
Jake <34009334+thermoabric@users.noreply.github.com>
2020-07-17 15:29:55 +0100

    view appointments for patient working !

--- src/DataHandling/dataReadIn.java
+++ src/DataHandling/dataReadIn.java
@@ -15,6 +15,8 @@ import java.util.ArrayList;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import userRequests.userPrescription;
+import userRequests.userAppointments;
 
 
 
@@ -34,6 +36,7 @@ public class dataReadIn {
     private ArrayList<user> doctorObj;
     private ArrayList<user> patientObj;
     private ArrayList<user> secretaryObj;
+    
     
     public void addPatientToArray(user a,ArrayList patientObj){
     
@@ -177,20 +180,23 @@ public class dataReadIn {
  
        
        
-      // user createDoctor = new doctorObj(username, password,firstname,lastname,mobilePhoneNumber,addressline,age,gender);
+      user createDoctor = new doctorObj(username, password,firstname,lastname,mobilePhoneNumber,addressline,age,gender);
        
-      // addPatientToArray(createDoctor, patientObj);
+      addDoctorToArray(createDoctor, doctorObj);
        
    }
   }
-  public void removeDoctor(user a, ArrayList doctor)
-    {
+  public void removeDoctor(user a, ArrayList doctor) {
         
         doctor.remove(a);
-    }   
+    } 
+  public void addDoctorToArray(user a, ArrayList doctor){
+        doctor.add(a);
+    }  
+  
   public void readCurrentSecretarys(ArrayList<user> secretaryObj) throws Exception {
   
-    File file = new File("./checks\\approvedSecretary.txt");
+    File file = new File("./checks\\approvedSecretarys.txt");
     
     BufferedReader br = new BufferedReader(new FileReader(file));
         String username;
@@ -200,29 +206,120 @@ public class dataReadIn {
         
         
         
+        
    while ((br.readLine()) != null){
        username = br.readLine();
        password = br.readLine();
        firstname = br.readLine();
        lastname = br.readLine();
+       
  
        
        
-      // user createDoctor = new doctorObj(username, password,firstname,lastname,mobilePhoneNumber,addressline,age,gender);
+      user createSecretary = new secretaryObj(username, password,firstname,lastname);
        
-      // addPatientToArray(createDoctor, patientObj);
+      addSecretaryToArray(createSecretary, secretaryObj);
+       
        
    }
   }
-  
 
-    public void removeSecretary(user a, ArrayList secretary)
-    {
-
+   public void removeSecretary(user a, ArrayList secretary){
         secretary.remove(a);
     }   
        
-   } 
+   public void addSecretaryToArray(user a, ArrayList secretary){
+        secretary.add(a);
+    }
+   public void addPrescription(userRequests.userPrescription a, ArrayList perscriptions)
+        {
+            
+            perscriptions.add(a);
+           
+        }
+    public void removePrescription(userRequests.userPrescription a, ArrayList perscriptions)
+    {
+       
+        perscriptions.remove(a);
+    }
+   public void readCurrentPresciptions(ArrayList<userPrescription> prescriptions) throws Exception {
+  
+    File file = new File("./checks\\prescriptions.txt");
+    
+    BufferedReader br = new BufferedReader(new FileReader(file)); 
+        
+        String username,password,firstname,lastname,addressline,gender,
+        age,patientnumber,doctorfirstname,doctorlastname,doctoraddressline,notes,meds,quantity,dosage;    
+        
+        while ((br.readLine()) != null) {
+             username = br.readLine();
+             password = br.readLine();
+             firstname = br.readLine();
+             lastname = br.readLine();
+             addressline = br.readLine();
+             gender = br.readLine();
+             age = br.readLine();
+             patientnumber = br.readLine();
+             doctorfirstname = br.readLine();
+             doctorlastname = br.readLine();
+             doctoraddressline = br.readLine();
+             notes = br.readLine();
+             meds = br.readLine();
+             quantity = br.readLine();
+             dosage = br.readLine();
+            
+            userRequests.userPrescription a = new userPrescription(username, password, firstname, lastname, addressline, gender,age,patientnumber,doctorfirstname,doctorlastname,doctoraddressline,notes,meds,quantity,dosage);
+            
+            addPrescription(a, prescriptions);
+        }
+       
+       
+   }
+   public void addAppointment(userRequests.userAppointments a, ArrayList appointments)
+        {
+            
+            appointments.add(a);
+           
+        }
+   public void removeAppointments(userRequests.userAppointments a, ArrayList appointments)
+    {
+       
+        appointments.remove(a);
+    }
+   public void readCurrentAppointments(ArrayList<userAppointments> appointments) throws Exception {
+  
+    File file = new File("./checks\\appointments.txt");
+    
+    BufferedReader br = new BufferedReader(new FileReader(file)); 
+        
+        String date, username,password,firstname,patientnumber,doctornumber,time;    
+        
+        while ((br.readLine()) != null) {
+             date = br.readLine();
+             username = br.readLine();
+             password = br.readLine();
+             firstname = br.readLine();
+             patientnumber = br.readLine();
+             doctornumber = br.readLine();
+             time = br.readLine();
+             
+            
+            userRequests.userAppointments a = new userAppointments(date,username, password, firstname,patientnumber,doctornumber,time);
+            addAppointment(a, appointments);
+        }
+   
+   
+   
+  }}
+   
+   
+   
+   
+   
+   
+   
+   
+ 
   
    
    
